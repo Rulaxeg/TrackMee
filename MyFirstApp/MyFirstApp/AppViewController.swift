0b57f0327eee9556c91de1b034537ab3f51740fb
Rulaxeg <rulax@live.com>
2022-11-21 18:23:12 -0600

    Primer commit
    Agregue cosas visual

---
+++ MyFirstApp/MyFirstApp/AppViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AppViewController.swift
+//  MyFirstApp
+//
+//  Created by Raul Espinoza on 03/11/22.
+//
+
+import Foundation
+import UIKit
+
+class AppViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
